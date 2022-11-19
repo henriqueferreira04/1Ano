49b7a2aa65196a3d81201f99f8ece82071e08bb7
henriqueferreira04 <ferreira.manuel.henrique04@ua.pt>
2022-11-19 15:57:30 +0000

    Feito

--- 1Semestre/FP/aula06/school.py
+++ 1Semestre/FP/aula06/school.py
@@ -2,13 +2,37 @@
 
 # a)
 def loadFile(fname, lst):
-    ...
+    with open(fname,'r') as file:
+        
+        for line in file:
+            student = []
+            campo = line.split('\t')
+            for n in campo:
+                if n.isdigit():
+                    n = int(n)
+                    student.append(n)
+                else:
+                    student.append(n)
+            student = tuple(student)
+            print(notaFinal(student))
+
+        
+
+            
     
 # b) Crie a função notaFinal aqui...
-...
+def notaFinal(student):
+    
+    nota1 = student[5]
+    nota2 = student[6]
+    nota3 = student[7]
+    notaFinal = (nota1 + nota2 + nota3)/3
+    return notaFinal
+
+
 
 # c) Crie a função printPauta aqui...
-...
+
 
 # d)
 def main():
@@ -17,6 +41,7 @@ def main():
     loadFile("school1.csv", lst)
     loadFile("school2.csv", lst)
     loadFile("school3.csv", lst)
+    
     
     # ordenar a lista
     ...
